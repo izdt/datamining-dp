47486f4d4726c701f569f906e866436c73fc8beb
zdt@macbook <dev@dthappy.com>
2017-03-29 17:48:47 +0800

    add multi-download method

--- node/dataDownloader.js
+++ node/dataDownloader.js
@@ -1,16 +1,34 @@
 const https = require('https');
+const fs = require('fs');
+
 
 const baseUrl = "https://takeaway.dianping.com/waimai/ajax/newm/newIndex";
+const jsonFile = "data/shopList.json";
+const offset = 20;
 
 const dataDownloader = {
-
+    savedDataPage:0,
+    currentData:[],
+    saveShopListJson(data){
+        this.currentData=[...this.currentData,...data];          
+        this.savedDataPage++;
+        if(this.savedDataPage>=offset){
+            console.log("save files");
+            fs.appendFile(jsonFile,JSON.stringify(this.currentData),(err)=>{
+                console.log(err);
+            })
+        }
+    },
     getShopList(data){
         let json = JSON.parse(data);
-        let shopList = json.data.shopList;
+        let shopList=[];
+        if(json.data&&json.data.shopList)
+        shopList = json.data.shopList;
+        console.log("get shoplist: "+shopList.length);
         return shopList;
     },
     download(startIndex,channel,lat,lng,geoType){
-        let fromUrl = baseUrl+"?startIndex="+startIndex+"&channel="+channel+"&lat="+lat+"&lng="+lng+"&geoType="+geoType+"&initialLat="+lat+"&initialLng="+lng;
+        let fromUrl = baseUrl+"?startIndex="+(++startIndex)+"&channel="+channel+"&lat="+lat+"&lng="+lng+"&geoType="+geoType+"&initialLat="+lat+"&initialLng="+lng;
         console.log(fromUrl);
         https.get(fromUrl,(res)=>{
             let dpData = '';
@@ -19,12 +37,18 @@ const dataDownloader = {
             });
             res.on('end', ()=> {
                 //console.log(dpData);
-                let ShopList = getShopList(dpData);
+                let shopList = this.getShopList(dpData);
+                this.saveShopListJson(shopList);
             });
         });
+    },
+    multiDownload(startIndex,channel,lat,lng,geoType){
+        for(let i=0;i<20;i++){
+            this.download(i,"6","39.99281","116.31088","2");
+        }
     }
 }
 
-dataDownloader.download("1","6","39.99281","116.31088","2");
+dataDownloader.multiDownload("1","6","39.99281","116.31088","2");
 
 module.exports = dataDownloader;

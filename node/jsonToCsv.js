f742d9bd185a3ad982c079ed51cf21ea54895947
izdt <dev@dthappy.com>
2017-04-02 15:10:09 +0800

    update jsonToCsv method and generate shopList.csv

--- node/jsonToCsv.js
+++ node/jsonToCsv.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
 const dataFolder = 'data';
+const shopCsvFile = 'data/shopList.csv';
 const shopJsonFile = /^shopList\d*\.json$/;
 const convertor = {
     appendToShopListCsv(data){
-        console.log(data.length);
+        let shopCsv = "";
+        for(let shop of data){
+            shopCsv+=shop.name+","
+            +shop.sold+","
+            +shop.star+","
+            +shop.deliverTime+","
+            +shop.minDeliverFee+","
+            +shop.minFee+","
+            +shop.brand+","
+            +shop.overtimePaymentSupported+","
+            +shop.isNewShopPromotion+","
+            +shop.extraService+","
+            +shop.mtWmPoiId+"\r\n";
+        }
+        fs.appendFile(shopCsvFile,shopCsv,(err)=>{
+            if(err) throw err;
+        });
     },
     shopListToCsv(){
         fs.readdir(dataFolder,(err,files)=>{

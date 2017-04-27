327f3f81675b5a8328a73179c5bd195b88409385
izdt <dev@dthappy.com>
2017-04-27 18:37:23 +0800

    update menuList

--- r/getStatisticWithMenu.r
+++ r/getStatisticWithMenu.r
@@ -3,5 +3,8 @@ setwd('../')
 
 #use ggplot2 library
 library(ggplot2)
+Sys.getlocale()
+Sys.setlocale(locale = "Chinese")
+menuTable <- read.csv('data/menuList.csv',encoding = 'UTF-8',sep =',',quote = "")
+#write.csv(menuTable, 'data/menu.csv', fileEncoding='UTF-8',quote = F)
 
-menuTable <- read.csv('data/menuList.csv',fileEncoding = 'UTF-8',sep =',',quote = "")

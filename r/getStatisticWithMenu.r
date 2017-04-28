98e2b28901f9a3c66ca0a54ed287625e3def8d5e
ZDT@VAIO <dev@dthappy.com>
2017-04-28 20:56:54 +0800

    update menu

--- r/getStatisticWithMenu.r
+++ r/getStatisticWithMenu.r
@@ -4,4 +4,4 @@ setwd('../')
 #use ggplot2 library
 library(ggplot2)
 
-menuTable <- read.csv('data/menuList.csv',fileEncoding = 'UTF-8',sep =',',quote = "")
+menuTable <- read.csv('data/menuList.csv',encoding = 'UTF-8',fileEncoding = 'UTF-8',sep =',',quote = "")
